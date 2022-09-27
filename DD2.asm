d3f3c2fe89c98b46b892ac6544361fc730070723
Robert. R <motley679@gmail.com>
2022-09-27 17:08:52 -0400

    Create DD2.asm

---
+++ DD2.asm
@@ -0,0 +1,30 @@
+format PE DLL
+entry DllEntryPoint
+
+include 'C:\Users\Robert\Desktop\Fasm, x86 ASM\INCLUDE\win32a.inc'
+
+;=================== main ====================
+
+section '.text' code readable executable
+
+proc DllEntryPoint hinstDLL,fdwReason,lpvReserved
+        mov     eax,TRUE
+        ret
+endp
+; Test
+proc SetCar
+        mov    eax, DWORD 0x467400      ; Some address
+        mov    BYTE [eax], 0x03         ; Some hex value
+       ret
+
+endp
+
+section '.edata' export data readable
+
+export 'DD2.DLL', SetCar,'SetCar'
+
+section '.reloc' fixups/fuckups data readable discardable
+
+  if $=$$
+    dd 0,8              ; if there are no fixups, generate dummy entry
+  end if
